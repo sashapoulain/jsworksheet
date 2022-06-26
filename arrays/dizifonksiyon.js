f0ff737fa0a1f984063813edc7d436413193cfc9
Sasha Poulain <sashapoulain5@gmail.com>
2022-06-26 15:25:39 +0300

    dizi içinde fonksiyon gösterildi

---
+++ arrays/dizifonksiyon.js
@@ -0,0 +1,10 @@
+let diziFonk = [
+    function sayHi() {
+        console.log("hi");
+    },
+    function sayBye() {
+        console.log("bye");
+    },
+];
+console.log(diziFonk[0]());
+console.log(diziFonk[1]());

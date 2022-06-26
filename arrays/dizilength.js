d329080fee86f44069d193fb59061a885abb9cc0
Sasha Poulain <sashapoulain5@gmail.com>
2022-06-26 15:18:26 +0300

    dizilere ekleme yapıldı ve gitignore oluşturuldu

---
+++ arrays/dizilength.js
@@ -0,0 +1,3 @@
+let dizi = ["şeyma", "mert", "süt"];
+console.log(dizi.length);
+console.log(dizi[dizi.length - 1]);
---
+++ arrays/diziyidönmek.js
@@ -0,0 +1,7 @@
+let dizi = ["şeyma", "mert", "süt"];
+dizi.forEach((element) => {
+    console.log(element);
+});
+for (let index = 0; index < dizi.length; index++) {
+    console.log(dizi[index]);
+}

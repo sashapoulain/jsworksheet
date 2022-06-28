150e7a9d1d453f039b6e3db82135a52c2ea32231
Sasha Poulain <sashapoulain5@gmail.com>
2022-06-28 16:35:20 +0300

    deneme çalışması gerçekleştirildi

---
+++ degiskenler/string2.js
@@ -0,0 +1,9 @@
+let a = null;
+console.log(a, typeof a);
+//null, object
+let b = "";
+console.log(b, typeof b);
+//"", string
+//o zaman
+console.log(null == "");
+//false çünkü biri object diğeri string

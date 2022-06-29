415496bf91c34417a34053515a58ad080d0f7e25
Sasha Poulain <sashapoulain5@gmail.com>
2022-06-29 21:18:44 +0300

    challenge düzenlendi

--- kararyapilari/deneme.js
+++ kararyapilari/deneme.js
@@ -1,29 +1,3 @@
-// "use strict";
-
-// process.stdin.resume();
-// process.stdin.setEncoding("utf-8");
-
-// let inputString = "";
-// let currentLine = 0;
-
-// process.stdin.on("data", function(inputStdin) {
-//     inputString += inputStdin;
-// });
-
-// process.stdin.on("end", function() {
-//     inputString = inputString.split("\n");
-
-//     main();
-// });
-
-// function readLine() {
-//     return inputString[currentLine++];
-// }
-
-// function main() {
-//     const N = parseInt(readLine().trim(), 10);
-// }
-
 /*
 n tekse Weird yaz
 n çiftse ve 2 ve 5 dahil aralığındaysa Not Weird yaz

6a6b5ff8a037e5cd2718a83981c332663fa0b019
Sasha Poulain <sashapoulain5@gmail.com>
2022-07-04 18:20:18 +0300

    ornek bitirildi ve localstorage konusuna giriş yapıldı

--- fonksiyonlar/js/ornek.js
+++ fonksiyonlar/js/ornek.js
@@ -6,8 +6,16 @@ azaltBtn.addEventListener("click", azalt);
 
 function arttir() {
     counter.innerHTML++;
+    // console.log(counter.innerHTML);
+    /*
+      localStorage.setItem("counter", JSON.stringify(counter.innerHTML));
+      let c = JSON.parse(localStorage.getItem("counter"));
+      console.log(c);
+      mesela bu locale kaydediyor ama son veriyi kaydediyor.
+      */
 }
 
 function azalt() {
     counter.innerHTML--;
+    // console.log(counter.innerHTML);
 }
---
+++ localstorage/js/local.js
@@ -0,0 +1,41 @@
+//locale veri nasıl kaydedilir
+/*
+localStorage.setItem("veri", JSON.stringify(veri));
+let veri = JSON.parse(localStorage.getItem("veri"));
+şeklinde 
+ama buradaki sıkıntı şu: en son yazılan veriyi kaydediyor.
+ornek.html ve ornek.js de ornekler mevcuttur.
+
+*/
+
+//localstorage ile çalışma orneği 1
+let d = document.querySelector("#deneme");
+let users = {
+    name: "Şeyma Yılmaz",
+    mail: "sashapoulain5@gmail.com",
+    isActive: true,
+};
+localStorage.setItem("user", JSON.stringify(users)); //bilgiyi yerleştir
+//localStorage.setItem("user", JSON.stringify(users)); buradaki user ile  let uInfo = localStorage.getItem("user");  buradaki user aynı
+let uInfo = localStorage.getItem("user"); //bilgiyi oku
+uInfo = JSON.parse(uInfo); //json formatına çevir
+d.innerHTML = uInfo.name; //html e yazdır
+console.log(uInfo);
+
+//local storage ile çalışma orneği 2
+let d2 = document.querySelector("#deneme2");
+//obje oluştur
+let kisi = {
+    isim: "mert",
+    hobby: "basketbol",
+    yas: 25,
+};
+//local storage e yazdır
+//html e yazdır
+// localStorage.setItem("person", JSON.stringify(kisi));
+// let person = localStorage.getItem("person");
+// person = JSON.parse(person);
+// d2.innerHTML = person.isim;
+localStorage.setItem("person", JSON.stringify(kisi));
+let person = JSON.parse(localStorage.getItem("person"));
+d2.innerHTML = person.isim;
